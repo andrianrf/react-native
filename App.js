a6145667a7723bbef3f039d445d2c179bb80121c
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-10-10 23:37:54 +0700

    add scrollview and padding

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, ScrollView, View } from 'react-native'
 import BasicComponent from './src/pages/sample/BasicComponent'
 import BasicStyling from './src/pages/sample/BasicStyling'
 
 export const App = () => {
   return (
-    <View>
+    <ScrollView>
+      <View style={{padding:20}}>
       <Text>Basic Component : </Text>
       <BasicComponent />
       <Text>Basic Styling : </Text>
       <BasicStyling />
-    </View>
+      </View>
+    </ScrollView>
   )
 }
 
