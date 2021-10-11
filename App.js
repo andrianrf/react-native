1556431293385f5ac7ff464586ded5a3ae4ac0ca
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-10-11 14:01:24 +0700

    add BasicPositioning

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Text, ScrollView, View} from 'react-native';
 import BasicComponent from './src/pages/sample/BasicComponent';
 import BasicLayouting from './src/pages/sample/BasicLayouting';
+import BasicPositioning from './src/pages/sample/BasicPositioning';
 import BasicStyling from './src/pages/sample/BasicStyling';
 
 export const App = () => {
   return (
     <ScrollView>
       <View style={{padding: 20}}>
+        <Text>Basic Positioning : </Text>
+        <BasicPositioning />
         <Text>Basic Layouting : </Text>
         <BasicLayouting />
         <Text>Basic Styling : </Text>
