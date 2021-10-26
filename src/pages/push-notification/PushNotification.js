2b4f71924af4e7addb0824212ab017e16488fb1d
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-10-27 00:29:18 +0700

    setup local push notification

---
+++ src/pages/push-notification/PushNotification.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function PushNotification() {
+    return (
+        <View>
+            <Text>npm install react-native-push-notification --save</Text>
+            <Text>https://github.com/zo0r/react-native-push-notification</Text>
+            <Text>console.firebase.google.com</Text>
+        </View>
+    )
+}
